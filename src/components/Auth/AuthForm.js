899503760a3202bf50a4be4453237958da0d1127
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-30 17:31:57 +0100

    connect form to firebase for create new account

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,39 +1,69 @@
-import { useState } from 'react';
+import {useRef, useState} from 'react';
+import {FIREBASE_API_KEY} from '../../utils/firebase';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+	const [isLogin, setIsLogin] = useState(true);
+	const emailInputRef = useRef();
+	const pwdInputRef = useRef();
+	const switchAuthModeHandler = () => {
+		setIsLogin((prevState) => !prevState);
+	};
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+	const submitHandler = (event) => {
+		event.preventDefault();
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+		const enteredEmail = emailInputRef.current.value;
+		const enteredPassword = pwdInputRef.current.value;
+		if (isLogin) {
+		} else {
+			fetch(
+				'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+					FIREBASE_API_KEY,
+				{
+					method: 'POST',
+					body: JSON.stringify({
+						email: enteredEmail,
+						password: enteredPassword,
+						returnSecureToken: true,
+					}),
+				}
+			).then((res) => {
+				if (res.ok) {
+				} else {
+					res.json().then((data) => {
+						console.log(data);
+					});
+				}
+			});
+		}
+	};
+
+	return (
+		<section className={classes.auth}>
+			<h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+			<form onSubmit={submitHandler}>
+				<div className={classes.control}>
+					<label htmlFor="email">Your Email</label>
+					<input type="email" id="email" ref={emailInputRef} required />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="password">Your Password</label>
+					<input type="password" id="password" ref={pwdInputRef} required />
+				</div>
+				<div className={classes.actions}>
+					<button>{isLogin ? 'Login' : 'Create Account'}</button>
+					<button
+						type="button"
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}>
+						{isLogin ? 'Create new account' : 'Login with existing account'}
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 };
 
 export default AuthForm;
