444818eba1692cead04b7cf6cf808186097cb8f9
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-30 18:37:05 +0100

    change passworld

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,48 @@
+import {useContext, useRef} from 'react';
+import AuthContext from '../../store/auth-context';
+import {FIREBASE_API_KEY} from '../../utils/firebase';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+	const newPwdEntered = useRef();
+	const authCtx = useContext(AuthContext);
+
+	const changePwdHandler = (event) => {
+		event.preventDefault();
+		const enteredNewPassword = newPwdEntered.current.value;
+		fetch(
+			'https://identitytoolkit.googleapis.com/v1/accounts:update?key=' +
+				FIREBASE_API_KEY,
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					idToken: authCtx.token,
+					password: enteredNewPassword,
+					returnSecureToken: true,
+				}),
+			}
+		).then((res) => {
+			if (!res.ok) {
+				console.log(res);
+			}
+		});
+	};
+	return (
+		<form className={classes.form} onSubmit={changePwdHandler}>
+			<div className={classes.control}>
+				<label htmlFor="new-password">New Password</label>
+				<input
+					type="password"
+					id="new-password"
+					minLength="7"
+					ref={newPwdEntered}
+				/>
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
+};
 
 export default ProfileForm;
